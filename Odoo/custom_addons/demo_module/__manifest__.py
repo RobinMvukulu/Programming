3c103e94a41bf3f84060ab67705a88295c0f2ecd
RobinMvukulu <mvukulur@gmail.com>
2025-08-20 14:25:11 +0200

    Testing Odoo, add Library Management

---
+++ Odoo/custom_addons/demo_module/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Demo Module",
+    "version": "1.0",
+    "summary": "Just a test module",
+    "category": "Tools",
+    "author": "Me",
+    "depends": [],
+    "data": [],
+    "installable": True,
+    "application": False,
+}
