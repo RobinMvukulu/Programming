c5f84dabc4da786246dd1cad05e8c2bdc513ea87
RobinMvukulu <mvukulur@gmail.com>
2025-08-29 15:18:26 +0200

    Odoo: Update Books from Library Module + use Sale and Purchase

--- Odoo/Library/addons/library/models/library_book.py
+++ Odoo/Library/addons/library/models/library_book.py
@@ -1,14 +1,23 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class LibraryBook(models.Model):
     _name = 'library.book'
-    _description = 'Livre'
+    _description = 'Book'
 
-    name = fields.Char(string='Titre', required=True)
-    author = fields.Char(string='Auteur')
-    isbn = fields.Char(string='ISBN')
-    price = fields.Float(string='Prix')
-    copies_available = fields.Integer(string='Exemplaires disponibles', default=0)
+    name = fields.Char('Title', required=True)
+    author = fields.Char('Author')
+    isbn = fields.Char('ISBN')
+    price = fields.Float('Price')
+    product_id = fields.Many2one('product.product', string='Related Product', domain="[('type','=','product')]")
+    copies_available = fields.Integer('Copies Available', related='product_id.qty_available', store=True)
 
-    purchase_ids = fields.One2many('purchase.order.line', 'product_id', string='Achats')
-    sale_ids = fields.One2many('sale.order.line', 'product_id', string='Ventes')
+    def create_product_for_book(self):
+        """Create a product for each book if not already linked."""
+        for book in self:
+            if not book.product_id:
+                product = self.env['product.product'].create({
+                    'name': book.name,
+                    'list_price': book.price,
+                    'type': 'product',
+                })
+                book.product_id = product
